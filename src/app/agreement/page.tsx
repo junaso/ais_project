05426cd2027f45474d9c6691e2493b8dbe2dd171
kimsh <kimsh@ais-info.co.jp>
2024-05-09 16:53:02 +0900

    create agreement page

---
+++ src/app/agreement/page.tsx
@@ -0,0 +1,10 @@
+import Page from 'app/page'
+import AgreementForm from 'components/organisms/pageDesign/AgreementForm'
+
+const AgreementPage = () => (
+  <Page title="Agreement Form">
+    <AgreementForm />
+  </Page>
+)
+
+export default AgreementPage
